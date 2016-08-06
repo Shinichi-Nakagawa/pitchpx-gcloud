38da28370eccdd84cbe7a1dee66fd7befbee59f8
shinyorke <spirits.is.my.rader@gmail.com>
2016-08-06 17:01:43 +0900

    必要なモノを諸々追加

--- pitchpx-gcloud/google.py
+++ pitchpx-gcloud/google.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import os
+import yaml
+
 __author__ = 'Shinichi Nakagawa'
 
 
 class _GoogleAPI(object):
 
-    def __init__(self, path):
+    def __init__(self, path, setting_file='/setting.yml'):
+        self.setting = yaml.load(open(self.DELIMITER.join([os.path.abspath(__file__), setting_file]), 'r'))
         self.path = path
 
 
